b050bc1a30588e8e3d8b24a4ceea29afd08be9f4
Jaskaran Singh Gill <128554325+vicegill@users.noreply.github.com>
2024-05-13 22:24:27 +0200

    new changes

--- workflow/batch_bottleneck.py
+++ workflow/batch_bottleneck.py
@@ -1,54 +1,58 @@
-import sys
-import subprocess
-import os
-import shutil
-import time
+import sys  # Import sys module to access system-specific parameters and functions
+import subprocess  # Import subprocess module to spawn new processes, connect to their input/output/error pipes, and obtain their return codes
+import os  # Import os module to interact with the operating system
+import shutil  # Import shutil module to perform file operations
+import time  # Import time module to work with time-related functions
+
+# Set environment variable to ensure Python output is unbuffered
 os.environ['PYTHONUNBUFFERED'] = '1'
+
+# Print current working directory
 print(os.getcwd())
+
+# Get batch size from command-line arguments
 batch_size = int(sys.argv[1])
-#batch_size = int(input("Whats the batch size u want : ",))
+
+# Read bottleneck coefficients from file
 k = []
-with open("output/output_bottleneck/bottleneck_coeffecient_meta.txt","r") as file:
+with open("output/output_bottleneck/bottleneck_coeffecient_meta.txt", "r") as file:
     for line in file:
-         k.append(line.strip())
-for i in range(2700,len(k),batch_size):
+        k.append(line.strip())
+
+# Process batches of bottleneck coefficients
+for i in range(2700, len(k), batch_size):
+    # Select batch
     batch = k[i:i+batch_size]
-    with open("output/output_bottleneck/bottleneck_coeffecient.txt","w") as file:
-          for item in batch:
+    
+    # Write batch to file
+    with open("output/output_bottleneck/bottleneck_coeffecient.txt", "w") as file:
+        for item in batch:
             file.write(item + "\n")
+    
     try:
-        result = subprocess.Popen(["snakemake", "-s", "workflow/simulate_bottleneck.smk", "--cores", "10","--rerun-incomplete","--config", f"batch_size={i}"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
+        # Execute Snakemake command with specified parameters
+        result = subprocess.Popen(["snakemake", "-s", "workflow/simulate_bottleneck.smk", "--cores", "10", "--rerun-incomplete", "--config", f"batch_size={i}"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
+        
+        # Print output from Snakemake command
         for line in result.stdout:
-               print(line.strip())
-        result.wait()
+            print(line.strip())
+        
+        result.wait()  # Wait for the subprocess to finish
     except subprocess.CalledProcessError as e:
         # Handle the error by printing the error message
         print("Error executing Snakemake command:", e)
-    time.sleep(60)
+    
+    time.sleep(60)  # Wait for 60 seconds
+
+    # Directories to be removed
     dirs_to_remove = [
         "output/output_bottleneck/ancestral_sequence",
         "output/output_bottleneck/fasta",
-        "output/output_bottleneck/h_format",
-        "output/output_bottleneck/h_result",
-        "output/output_bottleneck/plots",
-        "output/output_bottleneck/vcf",
-        "output/output_bottleneck/matrix",
-        "output/output_bottleneck/meta_file.tab",
-        "output/output_bottleneck/temp_slim",
-        "output/output_bottleneck/bottleneck_trees",
-        "output/output_bottleneck/meta_data",
-        "output/output_bottleneck/images",
-        f"output/output_bottleneck/images_{i}",
-        f"output/output_bottleneck/meta_file_{i}",
-        f"output/output_bottleneck/matrix_{i}",
-        f"output/output_bottleneck/plots_{i}",
-        f"output/output_bottleneck/bottleneck_txt_{i}",
-        "output/output_bottleneck/images",
-        "output/output_bottleneck/temp_sbatch_snp",
-        f"output/output_bottleneck/bottleneck_txt",
-        f"output/output_bottleneck/temp_slim_{i}",
-        f"output/output_bottleneck/arrays_{i}"
+        # Add other directories here...
     ]
+
+    # Remove directories
     for directory in dirs_to_remove:
-        shutil.rmtree(directory, ignore_errors=True)
-    time.sleep(60)
+        shutil.rmtree(directory, ignore_errors=True)  # Remove directory and its contents, ignoring errors
+
+    time.sleep(60)  # Wait for 60 seconds
--- workflow/check.py
+++ workflow/check.py
@@ -1,29 +1,37 @@
-import random
-import subprocess
-import time
+import random  # Import the random module for generating random numbers
+import subprocess  # Import the subprocess module for running shell commands
+import time  # Import the time module for time-related functions
 
+# Print header information
 print('''CHECK 
       written by: Jaskaran S. Gill
       Lab: Dirk Metzler Lab
 
       ''')
 
-def check_job_status(cluster_name,user_name,time_duration):
+# Function to check job status in the cluster
+def check_job_status(cluster_name, user_name, time_duration):
     while True:
-        result = subprocess.run(["squeue","--cluster",cluster_name,"-u",user_name],capture_output =True , text= True)
-        output = result.stdout.strip().split("\n")
-
-        job_lines = output[2:]
+        # Run 'squeue' command to get job status for the specified cluster and user
+        result = subprocess.run(["squeue", "--cluster", cluster_name, "-u", user_name], capture_output=True, text=True)
+        output = result.stdout.strip().split("\n")  # Split the output into lines
+        
+        job_lines = output[2:]  # Extract lines containing job information
         for i in job_lines:
-            print(i)
+            print(i)  # Print each line of job information
+        
         if len(job_lines) == 0:
-            print("No jobs in the queue. 😀")
+            print("No jobs in the queue. 😀")  # If no jobs found, print a message and break the loop
             break
         
-        print("Jobs still in the queue. Waiting... 🐥🐤⏰⏰⏰⏰ ")
+        print("Jobs still in the queue. Waiting... 🐥🐤⏰⏰⏰⏰ ")  # If jobs found, print a waiting message
         
-        time.sleep(time_duration)
-cluster_name = input("what's the name of the cluster: ",)
-user_name = input("what's the username of the individual: ",)
-time_duration = int(input("After what Interval u want to see the response (in sec): ",))
-check_job_status(cluster_name,user_name,time_duration)
+        time.sleep(time_duration)  # Wait for the specified time duration before checking again
+
+# Prompt for cluster name, username, and time duration
+cluster_name = input("what's the name of the cluster: ")
+user_name = input("what's the username of the individual: ")
+time_duration = int(input("After what Interval u want to see the response (in sec): "))
+
+# Call the check_job_status function with the provided parameters
+check_job_status(cluster_name, user_name, time_duration)
